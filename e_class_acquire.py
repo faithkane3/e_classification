793089607a20cae1fafd98c04ca4f983fe241625
Faith Kane <faith.x.kane@gmail.com>
2021-02-21 15:54:21 -0600

    Commit function change, helper functions

--- e_class_acquire.py
+++ e_class_acquire.py
@@ -19,30 +19,41 @@ def new_titanic_data():
     This function reads the titanic data from the Codeup db into a df,
     write it to a csv file, and returns the df.
     '''
+    # Create SQL query.
     sql_query = 'SELECT * FROM passengers'
+    
+    # Read in DataFrame from Codeup db.
     df = pd.read_sql(sql_query, get_connection('titanic_db'))
-    df.to_csv('titanic_df.csv')
+    
     return df
 
 
 
 def get_titanic_data(cached=False):
     '''
-    This function reads in titanic data from Codeup database if cached == False 
+    This function reads in titanic data from Codeup database if cached == False
     or if cached == True reads in titanic df from a csv file, returns df
     '''
     if cached == False or os.path.isfile('titanic_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
         df = new_titanic_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('titanic_df.csv')
+        
     else:
+        
+        # If csv file exists or cached == True, read in data from csv file.
         df = pd.read_csv('titanic_df.csv', index_col=0)
+        
     return df
 
 ###################### Acquire Iris Data ######################
 
 def new_iris_data():
     '''
-    This function reads the iris data from the Codeup db into a df,
-    writes it to a csv file, and returns the df.
+    This function reads the iris data from the Codeup db into a df.
     '''
     sql_query = """
                 SELECT species_id,
@@ -55,8 +66,10 @@ def new_iris_data():
                 JOIN species
                 USING(species_id)
                 """
+    
+    # Read in DataFrame from Codeup db.
     df = pd.read_sql(sql_query, get_connection('iris_db'))
-    df.to_csv('iris_df.csv')
+    
     return df
 
 
@@ -67,7 +80,16 @@ def get_iris_data(cached=False):
     or if cached == True reads in iris df from a csv file, returns df
     '''
     if cached == False or os.path.isfile('iris_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame
         df = new_iris_data()
+        
+        # Cache data
+        df.to_csv('iris_df.csv')
+        
     else:
+        
+        # If csv file exists or cached == True, read in data from csv file.
         df = pd.read_csv('iris_df.csv', index_col=0)
+        
     return df
